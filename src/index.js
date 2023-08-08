566323e165a697b0e3d5b892a5ee0bfd4a3f058e
Matheus Oliveira <matheusvp2@ucl.br>
2023-08-08 04:49:08 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Funcionando Gostosinho"
-    })
+app.get("/set-cookies", async (req, res) => {
+    res.cookie('session', 'user.access_token', { httpOnly: false, sameSite: 'none', secure: true });
+    res.cookie('session_refresh', 'user.refresh_token', { httpOnly: false, sameSite: 'none', secure: true });
+    res.json({ message: "Cookies set successfully" })
 })
 
+app.get("/get-cookies", async (req, res) => {
+    const cookies = req.cookies;
+    res.json(cookies);
+})
 
-var server_port = process.env.PORT || 5000;
+app.get("/remove-cookies", async (req, res) => {
+    res.clearCookie('session');
+    res.clearCookie('session_refresh');
+})
 
-app.listen(server_port, () => {
-    console.log("Server Inicializado")
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 })
